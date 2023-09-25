ad6cd4c37fc52dbca684037ed2e69f8788491b1c
Nytrock <savvazhirnov@gmail.com>
2023-09-25 19:03:35 +0300

    Add division project

---
+++ Division/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Division
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            float result;
+
+            int x, y;
+
+            x = 5;
+            y = 2;
+
+            result = Convert.ToSingle(x) / y;
+            Console.WriteLine(result);
+            Console.ReadKey();
+        }
+    }
+}
