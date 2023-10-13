0bb859dd9e62ac70e8156b2cd73501dc0c4d02b8
Nytrock <savvazhirnov@gmail.com>
2023-10-13 23:03:59 +0300

    Add projects with arrays

---
+++ 3 Hour/5-ArraySum/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5_ArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 2, 3, 4, 7, 8 };
+            int sum = 0;
+
+            for (int i = 0; i < array.Length; i++) { 
+                sum += array[i];
+            }
+
+            Console.WriteLine(sum);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 3 Hour/6-ArrayMax/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_ArrayMax
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = { 1, 3, 5, 8, 12, 2, 25 };
+            int maxElement = int.MinValue;
+
+            for (int i = 0; i < array.Length; i++) {
+                if (maxElement < array[i]) {
+                    maxElement = array[i];
+                }
+            }
+
+            Console.WriteLine(maxElement);
+            Console.ReadKey();
+        }
+    }
+}
