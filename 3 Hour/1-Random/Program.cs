27029af7efb64f658ce6676ee4aa8c82198df3d0
Nytrock <savvazhirnov@gmail.com>
2023-10-13 08:35:28 +0300

    Add practical projects with random

---
+++ 3 Hour/1-Random/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1_Random
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            while (true) {
+                int value = rand.Next(0, 10); // without 10
+                Console.WriteLine(value);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ 3 Hour/4-Console/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(5, 10);
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.BackgroundColor = ConsoleColor.White;
+
+            Console.Clear(); // сотрёт старый цвет фона
+
+            Console.WindowHeight = 10;
+            Console.WindowWidth = 50;
+
+            Console.WriteLine("У строки удалится [\b\b символ.");
+
+            Console.ReadKey();
+        }
+    }
+}
